388ef7cbcba00bbdcb53c47c64d6c5a3bde66919
Peter Boorman <pboorm@users.noreply.github.com>
2021-11-16 16:07:40 +0100

    Update xsim.py

--- xsim.py
+++ xsim.py
@@ -81,8 +81,8 @@ fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 30
 fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
 
 ## more here: https://plotly.com/python/configuration-options/
-config = {}#'staticPlot': True}
-st.plotly_chart(fig, use_container_width=True, config=config)
+#config = {'staticPlot': True}
+st.plotly_chart(fig, use_container_width=True)#, config=config)
     
 
 # st.sidebar.markdown("### Model outputs")
